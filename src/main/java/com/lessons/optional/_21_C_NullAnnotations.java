7e015ea86b9b4e9a0080705fca150f0f46aa7735
Syed Asghar <syed.asghar@waitrose.co.uk>
2020-12-05 13:33:30 +0000

    Add examples for null checking annotations

---
+++ src/main/java/com/lessons/optional/_21_C_NullAnnotations.java
@@ -0,0 +1,71 @@
+package com.lessons.optional;
+
+import com.lessons.optional._100_Utils.Engine;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/*
+ - There is no shortage of them:
+     - javax.annotation
+     - javax.validation.constraints
+     - android.support.annotation
+     - edu.umd.cs.findbugs.annotations
+     - org.eclipse.jdt.annotation
+     - org.jetbrains.annotations
+     - org.checkerframework.checker.nullness.qual
+     - lombok.NonNull
+ - Some are compile time, some retained at runtime
+ - They have different levels of support in different ides and setups
+ - There is little consistency
+ - Not sure I would not bother with them personally!
+ - I prefer to:
+     - Return Optional from service methods
+     - Return nulls from beans not Optional
+     - Protect params with Objects.requireNonNull()
+     - Consider the Null Object Pattern in limited cases
+*/
+public class _21_C_NullAnnotations {
+
+    // Parameter null detection
+
+    void main() {
+        // Generates a warning
+        doIt(null);
+        // No warning
+        doThis(null);
+    }
+
+    void doIt(@NotNull String name) { }
+    void doThis(String name) { }
+
+    // Method return null detection
+
+    void printGet() {
+        // Generates a warning
+        int l1 = get().length();
+
+        // No warning
+        int l2 = get2().length();
+    }
+
+    @Nullable
+    String get() {
+        return null;
+    }
+
+    String get2() {
+        return null;
+    }
+
+    // Can still be violated!
+
+    void b() {
+        // No warning!
+        a(new _100_Utils.Car(1000, null).getEngine());
+        // Warning
+        a(null);
+    }
+
+    void a(@NotNull Engine e) { }
+
+}
