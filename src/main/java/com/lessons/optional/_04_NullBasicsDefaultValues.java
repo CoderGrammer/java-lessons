b29f4ecd619ad61a605aa24c853a3819228e7bd9
CoderGrammer <codergrammer@gmail.com>
2020-12-22 20:42:32 +0000

    Tidy

--- src/main/java/com/lessons/optional/_04_NullBasicsDefaultValues.java
+++ src/main/java/com/lessons/optional/_04_NullBasicsDefaultValues.java
@@ -20,63 +20,83 @@ public class _04_NullBasicsDefaultValues {
      - This is not the same as uninitialized in a local var but you will get a compile
        error if you try to use it
     */
-    String s;
-    void refTypes() {
-        print(s); // null
 
-       // // Member uninitialized
-       // String o;
-       // print(o.getBytes());
-
-       // // Null (not uninitialized)
-       // String ooo = null;
-       // print(ooo.getBytes());
-    }
-
-    static int i; // Inits to null
-    int j; // Inits to null
-
-    void primitives() {
-        // Static and instance variables:
-        print(i); // null
-        print(j); // null
-    }
-
-    void primitivesLocal() {
-        /*
-         - Member variables:
-         - int l = null; // Illegal! Compile error!
-        */
-        int o;
-        /*
-         - print(l); // null
-         - print(o); // Illegal! Compile error!
-        */
-    }
-
+    /*
+     - Instance Object variables
+     - null is the default value for all object types
+    */
     String t;
     Integer g;
-    // null is the default value for all reference types
-    void defaultValue() {
+    void instanceVarsObjects() {
         print(t); // prints null
         print(g); // prints null
     }
 
-    static String x; // Inits to null
-    String y; // Inits to null
-
-    void objects() {
-        // Static and instance variables:
-        print(x); // null
-        print(y); // null
+    /*
+     - Instance primitive variables
+         - Default to sensible defaults
+         - They cannot be null!
+    */
+    int j;
+    void instanceVarsPrimitives() {
+        print(j);
     }
 
-    void objectsLocal() {
-        // Member variables:
-        String s = null;
-        String t;
-        print(s); // null
-        // print(t); // Illegal! Compile error!
+    /*
+     - Static Object variables
+        - Statics are class scoped so can't be declared in a method
+        - The same initialization properties as instance variables
+        - Default to null
+    */
+    static String s;
+    void staticObjects() {
+        print(s);
+    }
+
+    /*
+     - Static primitive variables
+        - Default to a sensible defaults, like instance variables
+    */
+    static int o;
+    void staticPrimitives() {
+        print(o);
+    }
+
+    /*
+     - Member Object variables
+         - Member variables are not initialized automatically
+         - You must initialize before use
+         - These are uninitialized NOT null
+    */
+    void memberVarsObjects() {
+        Object f;
+        // print(f.toString()); // Compile error
+    }
+
+    /*
+     - Member primitive variables
+        - Just like with objects these are uninitialized (not null)
+    */
+    void memberVarsPrimitives() {
+        int q;
+        // print(q); // Compile error
+        // int y = null; // Compile error
+    }
+
+    /*
+     - Member Object Variables Set to Null
+         - Not really useful for our discussion as they do not default to null
+    */
+    void memberVarsObjectsNull() {
+        Object o = null;
+    }
+
+    /*
+     - Member primitive Variables Set to Null
+         - Not really useful for our discussion as they do not default to null
+    */
+    void memberVarsPrimitivesNull() {
+        // int g = null; // Compile error!
     }
 
 }
