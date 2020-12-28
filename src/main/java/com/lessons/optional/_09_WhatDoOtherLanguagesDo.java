e7f66456c7761cc5ec61bb4e6d558590d35828b1
CoderGrammer <codergrammer@gmail.com>
2020-12-28 16:54:10 +0000

    Tidy

--- src/main/java/com/lessons/optional/_09_WhatDoOtherLanguagesDo.java
+++ src/main/java/com/lessons/optional/_09_WhatDoOtherLanguagesDo.java
@@ -1,10 +1,17 @@
 package com.lessons.optional;
 
 /*
+ - Some have library level support
+     - Example Java Optional
+ - Some have language level support
+     - Example Kotlin
+ - Some languages don't really have a concept of null and model the absence which a
+   Maybe type (not that dissimilar to Optional)
+     - Example Haskell
  - Some languages can perform 'sound null safety'
      - 'Sound' null safety offers more guarantees of null safety than the usual null
-       safety such that the compiler can
-       insert optimisations because it knows some variables can never be null
+       safety such that the compiler can insert optimisations because it knows some
+       variables can never be null
      - Example Dart
      - How is the 'soundness' achieved - with flow control analysis
      - e.g. in java I can call Optional.get() or just use any field that could be null
@@ -12,13 +19,6 @@ package com.lessons.optional;
      - In dart it checks the flow control and knows if you have structured code such
        that a nullable will not be null before calling it
      - We will see an example later
- - Some languages don't really have a concept of null and model the absence which a
-   Maybe type (not that dissimilar to Optional)
-     - Example Haskell
- - Some have language level support
-     - Example Kotlin
- - Some have library level support
-     - Example Java Optional
 */
 public class _09_WhatDoOtherLanguagesDo {
 }
